7758c459ead56b72ac948bdace78a9bd5baf402c
Noel Bryant <noelsobryant@gmail.com>
2024-08-04 00:24:42 +0300

    Enable typescript for runnind node application, update dockerfile run steps.

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,13 +1,21 @@
 import express, { Request, Response } from 'express';
-import { getAllTasks, getTask, createTask, updateTask, deleteTask } from '../controllers/tasks';
+import {
+  getAllTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
+} from '../controllers/tasks';
 
 const router = express.Router();
 
-router.route('/')
+router
+  .route('/')
   .get((req: Request, res: Response) => getAllTasks(req, res))
   .post((req: Request, res: Response) => createTask(req, res));
 
-router.route('/:id')
+router
+  .route('/:id')
   .get((req: Request, res: Response) => getTask(req, res))
   .patch((req: Request, res: Response) => updateTask(req, res))
   .delete((req: Request, res: Response) => deleteTask(req, res));
